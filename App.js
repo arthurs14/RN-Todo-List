8447f7724d4090491f06ac8244ea795690fb6c06
arthurs14 <arthurcs426@gmail.com>
2020-03-23 18:18:14 -0700

    touching todo item should delete, and be able to add a new todo to the list

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, FlatList } from 'react-native';
 
 import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 const App = () => {
   const [todos, setTodos] = useState([
@@ -10,15 +12,28 @@ const App = () => {
     { text: 'code code code', key: '3' },
   ]);
 
+  const pressHandler = key => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.key !== key));
+  };
+
+  const submitHandler = text => {
+    setTodos(prevTodos => [
+      { text, key: Math.random().toString() },
+      ...prevTodos,
+    ]);
+  };
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
